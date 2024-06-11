2059a591dc13fad8e6133428b20ff0272fdf090b
Saikiran Prodaturu <sprodaturu@gmail.com>
2024-06-11 03:23:23 +0200

    optimized for use  in google colab

--- main.py
+++ main.py
@@ -43,7 +43,40 @@ for i in range(0, len(text) - SEQ_LENGTH, STEP_SIZE):
 #* converting the lists into numpy arrays
 
 # create a 3D array of zeros with shape -> "number of sentences" x "sequence length" x "number of characters"
-x = np.zeros((len(sentences), SEQ_LENGTH, len(characters)), dtype=bool)
+x = np.zeros((len(sentences), SEQ_LENGTH, len(characters)), dtype=np.bool)
 
 # create a 2D array of zeros with shape -> "number of sentences" x "number of characters"
-y = np.zeros((len(sentences), len(characters)), dtype=bool)
+y = np.zeros((len(sentences), len(characters)), dtype=np.bool)
+
+for i, sentence in enumerate in sentences:
+	for t, char in enumerate(sentence):
+		x[i, t, char_2_index[char]] = 1
+	y[i, char_2_index[next_chars[i]]] = 1
+
+#> --- --- --- Creating the model for the first time --- --- --- <#
+# # uncomment lines below if training for the first time (leave # # in place)
+
+model = Sequential()
+model.add(LSTM(128, input_shape=(SEQ_LENGTH, len(characters))))
+model.add(Dense(len(characters)))
+model.add(Activation('softmax'))
+
+#* Compiling the model
+#	loss: loss function
+#	optimizer: optimizer
+#	metrics: list of metrics to be evaluated and displayed
+
+model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.01))
+#X model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.01), metrics=['accuracy'])
+
+#* fitting the model
+
+model.fit(x, y, batch_size=128, epochs=3)
+#x model.fit(x, y, batch_size=256, epochs=4)
+
+model.save('text_gen.model')
+
+#> --- --- --- Loading the model --- --- --- <#
+
+model = tf.keras.models.load_model('text_gen.model')
+
